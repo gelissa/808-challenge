d31344614f431321df11014e6c659ca1e19d8fcc
Gelissa Leveille <gelissaleveille@yahoo.com>
2021-09-12 19:36:01 -0400

    trying to render some keys

--- src/app/row/row.component.ts
+++ src/app/row/row.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ButtonComponent } from '../button/button.component'
 
 @Component({
   selector: 'app-row',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RowComponent implements OnInit {
 
-  constructor() { }
+  @Input() elements = [
+    { id: 1, value: 'q' },
+    { id: 2, value: 'w' },
+    { id: 3, value: 'e' },
+    { id: 4, value: 'r' },
+    { id: 5, value: 't' },
+    { id: 6, value: 'y' },
+    { id: 7, value: 'u' },
+    { id: 8, value: 'i' },
+    { id: 9, value: 'o' },
+    { id: 10, value: 'p' },
+    { id: 11, value: 'a' },
+    { id: 12, value: 's' },
+    { id: 13, value: 'd' },
+    { id: 14, value: 'f' },
+    { id: 15, value: 'g' },
+    { id: 16, value: 'h' },
+  ]; 
 
-  ngOnInit(): void {
+
+ makeRow(): typeof ButtonComponent {
+  for (let i = 0; i > 16; i++) {
+    return ButtonComponent;
   }
+}
+
+constructor() { 
+
+}
+
+ngOnInit(): void {
+}
 
 }
